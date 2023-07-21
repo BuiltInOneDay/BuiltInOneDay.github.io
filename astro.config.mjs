1d47a047e348df8e1fd7456c363baf4e6e330358
Anthony Ellsworth <anthonye629@gmail.com>
2023-07-20 19:00:30 -0700

    UPDATE - astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,6 @@ import image from "@astrojs/image";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://builtinoneday.github.io/',
-  base: '/BuiltInOneDay.github.io',
   integrations: [mdx(), sitemap(), tailwind(), image(
     {
       serviceEntryPoint: '@astrojs/image/sharp',
