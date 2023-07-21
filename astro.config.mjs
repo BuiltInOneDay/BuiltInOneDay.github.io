ab359630614b4e405362d8e1e7a0224fbef9b1ca
Anthony Ellsworth <anthonye629@gmail.com>
2023-07-20 18:46:30 -0700

    UPDATE - fix for astro deploy and github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,8 @@ import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
+  site: 'https://builtinoneday.github.io/',
+  base: '/BuiltInOneDay.github.io',
   integrations: [mdx(), sitemap(), tailwind(), image(
     {
       serviceEntryPoint: '@astrojs/image/sharp',
