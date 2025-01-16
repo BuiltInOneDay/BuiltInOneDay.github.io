cd7f4e9b8684167314242455ff39f3d87eaced42
Anthony <anthonye629@gmail.com>
2025-01-16 12:55:29 -0800

    UPDATE - Installed Partytown for GA tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,20 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
+import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://builtinoneday.github.io/',
   base: '/',
-  integrations: [mdx(), sitemap(), tailwind()]
+  integrations: [
+    mdx(), 
+    sitemap(), 
+    tailwind(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      }
+    })
+  ]
 });
